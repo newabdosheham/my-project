95c45c2b48a51683a1b21e8607274a62a02e1270
newabdosheham <abd2007sh@hotmail.com>
2024-12-25 14:24:22 +0200

    first commit

---
+++ terraform.tf
@@ -0,0 +1,37 @@
+terraform {
+
+  backend "remote" {
+  hostname = "app.terraform.io"
+  organization = "abdoshheam-deloitte"
+  workspaces {
+    name = "my-first-project"
+             }
+   }
+
+
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  
+
+  }
+}
